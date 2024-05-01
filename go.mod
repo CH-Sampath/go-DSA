f647e854258716e76e1bce51659004e20bdcc063
sampath <sampathsaicharan59@gmail.com>
2024-05-01 15:52:42 +0530

    Intial commit of stacks only

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CH-Sampath/go-DSA
+
+go 1.22.2
---
+++ stacks/stack1.go
@@ -0,0 +1,58 @@
+package stack
+
+type Stack struct {
+	Capacity int
+	Array    []string
+	Top      int
+}
+
+var Max_stack int = 10
+
+func CreateStack(s **Stack) {
+	*s = new(Stack)
+	(*s).Capacity = Max_stack
+	(*s).Array = make([]string, (*s).Capacity)
+	(*s).Top = -1
+}
+
+func IsEmpty(s **Stack) bool {
+	if (*s).Top == -1 {
+		return true
+	}
+	return false
+}
+
+func IsFull(s **Stack) bool {
+	if (*s).Top == (*s).Capacity-1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func PopStack(s **Stack) string {
+	if IsEmpty(s) {
+		return "-1"
+	} else {
+		ele := (*s).Array[(*s).Top]
+		(*s).Array[(*s).Top] = ""
+		(*s).Top -= 1
+		return ele
+	}
+}
+
+func DoubleSize(s **Stack) {
+	new_cap := (*s).Capacity * 2
+	newArray := make([]string, new_cap)
+	copy(newArray, (*s).Array)
+	(*s).Array = newArray
+	(*s).Capacity = new_cap
+}
+
+func PushStack(s **Stack, ele string) {
+	if IsFull(s) {
+		DoubleSize(s)
+	}
+	(*s).Array[(*s).Top+1] = ele
+	(*s).Top += 1
+}
